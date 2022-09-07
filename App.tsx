a432cf3475cec02c7e1d220fe53ce0fe70d75219
Nick Miller <nick@evvnt.com>
2022-09-07 16:45:14 -0400

    Callback kabooms!

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import Honeybadger from '@honeybadger-io/react-native';
 
 export default function App() {
   Honeybadger.configure('hbp_xyz');
-  nonexistentFunction();
+  nonexistentFunction_outside(); // no backtrace
+
+  function onClick() {
+    nonexistentFunction_callback(); // backtrace unless source maps were uploaded
+  }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button onPress={onClick} title="Click" />
     </View>
   );
 }
